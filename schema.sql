dfe6efb8fdce3b9a73f15c4eb1ab0a0c6bfc8a65
arvind kumar <Arvind.Kumar@mrcooper.com>
2020-07-04 14:15:38 +0530

    sql sp

--- schema.sql
+++ schema.sql
@@ -1,22 +1,30 @@
 CREATE TABLE RFID_RESPONSE
 (
     ID                           INT PRIMARY KEY IDENTITY (1, 1),
-    SENDER                       VARCHAR(8)  NOT NULL,
-    RECEIVER                     VARCHAR(8)  NOT NULL,
-    COMMUNICATION_POINT          VARCHAR(18) NOT NULL,
-    HANDSHAKE                    VARCHAR(2)  NOT NULL,
-    SEQUENCE_NUMBER              VARCHAR(20) NOT NULL,
-    ERROR                        VARCHAR(4)  NOT NULL,
-    TELEGRAM_TYPE                VARCHAR(4)  NOT NULL,
-    MESSAGE_TYPE                 VARCHAR(5)  NOT NULL,
-    WAREHOUSE_NUMBER             VARCHAR(4)  NOT NULL,
-    HANDLING_UNIT_IDENTIFICATION VARCHAR(20) NOT NULL,
-    MATERIAL_NUMBER              VARCHAR(18) NOT NULL,
-    BATCH                        VARCHAR(10) NOT NULL,
-    STOCK_TYPE                   VARCHAR(2)  NOT NULL,
-    TELEGRAM_ORDER               VARCHAR(12) NOT NULL,
-    TELEGRAM_STATUS              VARCHAR(3)  NOT NULL,
-    SOURCE_STORAGE_BIN           VARCHAR(18) NOT NULL,
-    ERROR_MESSAGE                VARCHAR(74) NOT NULL,
-    RECEIVED_ON                  DATETIME    NOT NULL,
+    SENDER                       VARCHAR(8),
+    RECEIVER                     VARCHAR(8),
+    COMMUNICATION_POINT          VARCHAR(18),
+    HANDSHAKE                    VARCHAR(2),
+    SEQUENCE_NUMBER              VARCHAR(20),
+    ERROR                        VARCHAR(4),
+    TELEGRAM_TYPE                VARCHAR(4),
+    MESSAGE_TYPE                 VARCHAR(5),
+    WAREHOUSE_NUMBER             VARCHAR(4),
+    HANDLING_UNIT_IDENTIFICATION VARCHAR(20),
+    MATERIAL_NUMBER              VARCHAR(18),
+    BATCH                        VARCHAR(10),
+    STOCK_TYPE                   VARCHAR(2),
+    TELEGRAM_ORDER               VARCHAR(12),
+    TELEGRAM_STATUS              VARCHAR(3),
+    SOURCE_STORAGE_BIN           VARCHAR(18),
+    ERROR_MESSAGE                VARCHAR(74),
+    RECEIVED_ON                  DATETIME,
 );
+
+CREATE PROCEDURE sp_delete_rfid_response
+AS
+BEGIN
+    DELETE
+    FROM RFID_RESPONSE
+    WHERE DATEDIFF(DAY, RECEIVED_ON, GETDATE()) > 90
+END
